0a3077516079622c4b9a2ff8a965051918fdb6e0
inagaki-a <67504211+inagaki-a@users.noreply.github.com>
2022-07-08 20:59:02 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -7,3 +7,17 @@ ham.addEventListener('click', function () {
   nav.classList.toggle('menu-open');
 
 });
+
+const targets = document.querySelectorAll('.scrollAnimation');
+
+window.addEventListener('scroll', function() {
+  const scroll = window.scrollY;
+  const height = window.innerHeight;
+  for(let target of targets) {
+    const element = target.getBoundingClientRect().top + scroll;
+    if (scroll > element - height) {
+      target.classList.add('start-animation');
+    }
+  }
+
+});
